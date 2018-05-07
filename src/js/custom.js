fdaaf78e3b68ea3e56657d22592a5b642ff122c4
romzukov <romanzukov1993@gmail.com>
2018-05-07 12:40:38 +0200

    Added work section and it styles

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,3 +6,18 @@ $(function () {
     // Animate on scroll
     new WOW().init();
 });
+
+/**
+ * Work
+ **/
+
+$(function () {
+    // Magnific popup
+    $('#work').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
